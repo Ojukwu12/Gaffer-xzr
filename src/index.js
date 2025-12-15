18898e179d6c92bcde74c71396b4c6a03276758a
Ojukwu12 <165922555+Ojukwu12@users.noreply.github.com>
2025-12-15 01:14:01 +0000

    Fix: Update startup call from initializeTransporter to initializeResend

--- src/index.js
+++ src/index.js
@@ -189,7 +189,7 @@ const startServer = async () => {
     llmService.initializeClient();
     
     const emailService = require('./services/emailService');
-    emailService.initializeTransporter();
+    emailService.initializeResend();
     
     const webPushService = require('./services/webPushService');
     webPushService.initializeWebPush();
